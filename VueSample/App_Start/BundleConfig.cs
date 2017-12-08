58683ba2ec7512bcd4416d90c07a6f1d3fbce641
Samuel Carvalho Locatelli <samuel.locatelli@apisul.com.br>
2017-12-08 10:51:29 -0200

    Primeiro commit do projeto

---
+++ VueSample/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Optimization;
+
+namespace VueSample
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundle(BundleCollection bundle)
+        {
+            bundle.Add(new ScriptBundle("~/Scripts").Include(
+                "~/Scripts/jquery-3.2.1.min.js",
+                "~/Scripts/materialize.min.js",
+                "~/Scripts/vue.min.js",
+                "~/Scripts/vue-resource.min.js",
+                "~/Scripts/index.js"
+            ));
+
+            bundle.Add(new StyleBundle("~/Styles/css").Include(
+                "~/Content/css/materialize.min.css"
+            ));
+        }
+    }
+}
